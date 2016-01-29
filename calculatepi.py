0e598fb54dcb81eac2a4bb149b722ab2a984a50b
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-01-29 15:03:44 -0500

    Updated from Brython Server: 1/29/2016 3:03:44 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: morgan
-Credit: <list sources used, if any>
+Author: daniel wilson
+Credit: morgan melement, mr. dennison
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
@@ -26,11 +26,3 @@ n = int(input("I will estimate pi. How many terms should I use? "))
 k = int(input("How many decimal places should I use in the result? "))
 pi = 4*sum([(-1.0)**k/(2*k+1) for k in range(0,n)])
 print("The approximate value of pi is {0}".format(round(pi, k)))
-
-
-
-"""
-e = 1.0/sum([((-1.0)**k)/math.factorial(k) for k in range(0,n)])
-print("The approximate value of e is {0}".format(round(e, decimals)))
-print("(The true value of e is {0})".format(round(math.e, decimals)))
-"""
